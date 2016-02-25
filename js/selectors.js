9678b9551ba035dae8027722e584cb661bdcfb85
Nicki Anselmo <nickianselmo@gmail.com>
2016-02-25 15:33:13 +0000

    initial commit - solution branch

--- js/selectors.js
+++ js/selectors.js
@@ -6,3 +6,22 @@ function pSelector(){
 }
 */
 
+function h1Selector(){
+  return $('h1');
+}
+
+function liInOlSelector(){
+  return $('ol li');
+}
+
+function linkSelector(){
+  return $('#my-id.da-class a');
+}
+
+function imageSelector(){
+  return $('img[alt="cat sleeping"]');
+}
+
+function checkboxInputSelector(){
+  return $('input[type="checkbox"]');
+}
