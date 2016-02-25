1ed88f483c29876b996c6cf8322fc9d30915fa18
Nicki Anselmo <nickianselmo@gmail.com>
2016-02-25 15:31:57 +0000

    took out solution

--- js/selectors.js
+++ js/selectors.js
@@ -6,22 +6,3 @@ function pSelector(){
 }
 */
 
-function h1Selector(){
-  return $('h1');
-}
-
-function liInOlSelector(){
-  return $('ol li');
-}
-
-function linkSelector(){
-  return $('#my-id.da-class a');
-}
-
-function imageSelector(){
-  return $('img[alt="cat sleeping"]');
-}
-
-function checkboxInputSelector(){
-  return $('input[type="checkbox"]');
-}
