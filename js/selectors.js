c41af9ed04900b592939f4a2a3a5875d8992a0b7
rclauson <rclauson@google.com>
2016-05-20 15:52:30 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -5,4 +5,19 @@ function pSelector(){
   return $('p');
 }
 */
+function h1Selector(){
+  return $('h1')
+}
+function liInOlSelector(){
+  return $("ol li")
+}
 
+function linkSelector(){
+return $("#my-id.da-class a")
+}
+function catSelector (){
+return $('img[alt="cat sleeping"]')
+}
+function checkboxInputSelector(){
+  return $("input[type='checkbox']")
+}
